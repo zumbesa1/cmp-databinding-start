c6ee605955a0ae2e8a6cf36dc09df2fe9f30d789
zumbesa1 <salahudin.z@hotmail.com>
2023-01-18 00:27:27 +0100

    add different Angular Hooks
    Hooks makes it possible to run code during the Lifecycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,4 +25,11 @@ export class AppComponent {
     });/**/
   }
 
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed!';
+
+  }
+  onDestroyFirst() {
+    this.serverElements.splice(0,1);
+  }
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +6,55 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   //encapsulation: ViewEncapsulation.None --> to make styles of this component globaly
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: {type: string, name: string, content: string};
+export class ServerElementComponent implements OnInit , 
+OnChanges, 
+DoCheck, 
+AfterContentInit, 
+AfterContentChecked, 
+AfterViewInit, 
+AfterViewChecked, OnDestroy{
+  @Input('srvElement') element: { type: string, name: string, content: string };
+  @Input() name: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor called');
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    //this is called whenever a Change occure in Angular
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
+
+  ngDoCheck(): void {
+    //this is called whenever Angular checks for any Changes
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit(): void {
+    //this is called only ones. It is called wenn the Content is finally initialised
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked(): void {
+    //this is called whenever Angular checks the content for the last time
+    console.log('ngAfterContentChecked called');
+  }
+  ngAfterViewInit(): void {
+    //this is called oafter the content has been checked
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked(): void {
+    //this is called oafter the content has been checked
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called');
+  }
 }
